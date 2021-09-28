a1e5b417643677d9d6fd12ba774a52d747bf2150
jog068 <32498827+jog068@users.noreply.github.com>
2021-09-27 22:13:51 -0500

    Product Description Fixed 1

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from "@angular/core";
 import { Http, Response } from "@angular/http";
 import { Observable } from "rxjs";
-import { map } from "rxjs/operator/map";
+import "rxjs/add/operator/map";
 
 @Injectable()
 export class ProductService {
